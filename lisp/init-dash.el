9c134e327fcd7835074b5539adbf756689f9f47a
Paul B <paul.bonaud@capitainetrain.com>
2018-09-03 22:34:28 +0200

    Install helm-dash package when not on a mac for Dash docsets usage

--- lisp/init-dash.el
+++ lisp/init-dash.el
@@ -8,10 +8,8 @@
   (require-package 'dash-at-point)
   (global-set-key (kbd "C-c D") 'dash-at-point))
 
-(when (and (not *is-a-mac*) (not (package-installed-p 'helm-dash)))
-  (require-package 'helm-dash))
-
-(when (package-installed-p 'helm-dash)
+(when (not *is-a-mac*)
+  (require-package 'helm-dash)
   (global-set-key (kbd "C-c D") 'helm-dash-at-point)
   (setq helm-dash-browser-func 'eww))
 
