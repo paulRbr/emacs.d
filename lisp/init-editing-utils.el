f39e7a27b3f5993d720e3ad9f901baaa3e7a7b9e
Paul B <paul@bonaud.fr>
2018-10-01 15:32:42 +0200

    ediff: Add a keyboard shortcut to copy A&B to C in ediff

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -317,6 +317,17 @@ With arg N, insert N newlines."
 
 
 
+;;----------------------------------------------------------------------------
+;; ediff: copy both A and B to C buffers new shortcut.
+;;----------------------------------------------------------------------------
+(defun ediff-copy-both-to-C ()
+  (interactive)
+  (ediff-copy-diff ediff-current-difference nil 'C nil
+                   (concat
+                    (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+                    (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+(defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
+(add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map)
 
 (require-package 'highlight-escape-sequences)
 (add-hook 'after-init-hook 'hes-mode)
