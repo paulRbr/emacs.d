0fec5f5ffdd7a94c8f0108f5f047546ced735bda
Paul B <paul@bonaud.fr>
2018-10-01 15:31:53 +0200

    terraform: Add hook to call 'terraform fmt' on terraform files

--- lisp/init-terraform.el
+++ lisp/init-terraform.el
@@ -7,7 +7,14 @@
 (when (maybe-require-package 'terraform-mode)
   (when (maybe-require-package 'company-terraform)
     (after-load 'terraform-mode
-      (company-terraform-init))))
+      (company-terraform-init)))
+
+  (defun terraform-fmt ()
+    "Format *.tf terraform files with terraform fmt."
+    (when (eq major-mode 'terraform-mode)
+      (shell-command-to-string "tf-make fmt")))
+
+  (add-hook 'after-save-hook #'terraform-fmt))
 
 (provide 'init-terraform)
 ;;; init-terraform.el ends here
