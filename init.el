3e044e252f84bb9797574750a3b363c0729059b1
Steve Purcell <steve@sanityinc.com>
2019-04-04 10:58:17 +1300

    "scratch" package now requires Emacs 25.1

--- init.el
+++ init.el
@@ -51,7 +51,7 @@
 
 (require-package 'wgrep)
 (require-package 'diminish)
-(require-package 'scratch)
+(maybe-require-package 'scratch)
 (require-package 'command-log-mode)
 
 (require 'init-frame-hooks)
