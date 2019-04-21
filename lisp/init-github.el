7c36b0a3b7cf5b3f89532cfc12bd1ea4196befc3
Paul B <paul@bonaud.fr>
2019-04-21 13:29:50 +0200

    magit(forge): add forge module and configuration

--- lisp/init-github.el
+++ lisp/init-github.el
@@ -1,4 +1,4 @@
-;;; init-github.el --- Github integration -*- lexical-binding: t -*-
+;;; Init-github.el --- Github integration -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
 
@@ -9,7 +9,14 @@
 (add-hook 'prog-mode-hook 'bug-reference-prog-mode)
 
 (maybe-require-package 'github-clone)
-(maybe-require-package 'magithub)
+(maybe-require-package 'forge)
+(define-key global-map (kbd "C-x p") 'forge-pull)
+(with-eval-after-load 'magit
+  (require 'forge)
+
+  (add-to-list 'forge-alist '("framagit.org" "framagit.org/api/v4" "framagit.org" forge-gitlab-repository))
+  (add-to-list 'forge-alist '("gitlab-perso.com" "gitlab-perso.com/api/v4" "gitlab-perso.com" forge-gitlab-repository))
+  (add-to-list 'forge-alist '("github-perso.com" "api.github-perso.com" "github-perso.com" forge-github-repository)))
 
 (provide 'init-github)
 ;;; init-github.el ends here
