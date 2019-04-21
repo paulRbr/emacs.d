8581acc1c802095f9643df800553ab487f2d1b1b
Paul B <paul@bonaud.fr>
2019-04-21 13:29:50 +0200

    magit(forge): add forge module and configuration

--- lisp/init-github.el
+++ lisp/init-github.el
@@ -1,4 +1,4 @@
-;;; init-github.el --- Github integration -*- lexical-binding: t -*-
+;;; Init-github.el --- Github integration -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
 
@@ -10,7 +10,12 @@
 
 (maybe-require-package 'github-clone)
 (maybe-require-package 'forge)
-(maybe-require-package 'github-review)
+(define-key global-map (kbd "C-x p") 'forge-pull)
+(with-eval-after-load 'magit
+  (require 'forge)
+
+  (add-to-list 'forge-alist '("gitlab-perso.com" "gitlab-perso.com/api/v4" "gitlab-perso.com" forge-gitlab-repository))
+  (add-to-list 'forge-alist '("github-perso.com" "api.github-perso.com" "github-perso.com" forge-github-repository)))
 
 (when (maybe-require-package 'flymake-actionlint)
   (add-hook 'yaml-mode-hook 'flymake-actionlint-action-load-when-actions-file))
