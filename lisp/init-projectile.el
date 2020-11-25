d4f445d508dfd5d549bf78d82563318339ec0439
Paul B <paul@bonaud.fr>
2020-11-25 10:39:40 +0100

    projectile: disable persp-projectile which seems buggy with emacs 26
    Instead I load persp-mode manually when I need

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -16,14 +16,26 @@
 
   (maybe-require-package 'ibuffer-projectile)
 
-  (after-load 'projectile
-    (when (maybe-require-package 'persp-projectile)
-      (persp-mode)
-      (require 'persp-projectile)
-      ;;   (define-key projectile-mode-map
-      ;;     (kbd "C-c p p")
-      ;;     'projectile-persp-switch-project))
-      )))
+  ;; (after-load 'projectile
+  ;;   ;; (when (maybe-require-package 'persp-projectile)
+  ;;   (persp-mode)
+  ;;   ;; (require 'persp-projectile)
+  ;; (define-key projectile-mode-map
+  ;;   (kbd "C-c p p")
+  ;;   'projectile-persp-switch-project)
+  ;;   ;; )
+  ;;   )
+  ;; (after-load 'projectile
+  ;;   (when (maybe-require-package 'persp-mode)
+  ;;     ;; (define-key persp-key-map (kbd "C-x x") ...)
+  ;;     (persp-mode)
+  ;;     (with-eval-after-load "persp-mode"
+  ;;       (setq wg-morph-on nil)
+  ;;       (setq persp-autokill-buffer-on-remove 'kill-weak)
+  ;;       (add-hook 'window-setup-hook #'(lambda () (persp-mode 1))))
+  ;;     (require 'persp-mode)
+  ;;     ))
+  )
 
 (provide 'init-projectile)
 ;;; init-projectile.el ends here
