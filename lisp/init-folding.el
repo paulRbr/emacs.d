965bfdd0166579a0f6d87d27de490f5ddb4cd1a1
Paul B <paul@bonaud.fr>
2020-11-25 10:38:58 +0100

    folding: add yafolding

--- lisp/init-folding.el
+++ lisp/init-folding.el
@@ -7,6 +7,16 @@
     (define-key origami-mode-map (kbd "C-c f") 'origami-recursively-toggle-node)
     (define-key origami-mode-map (kbd "C-c F") 'origami-toggle-all-nodes)))
 
+(when (maybe-require-package 'yafolding)
+  (after-load 'yafolding
+    (global-set-key (kbd "C-c y") 'yafolding-discover)
+    (defvar yafolding-mode-map
+      (let ((map (make-sparse-keymap)))
+        (define-key map (kbd "<C-S-return>") #'yafolding-hide-parent-element)
+        (define-key map (kbd "<C-M-return>") #'yafolding-toggle-all)
+        (define-key map (kbd "<C-return>") #'yafolding-toggle-element)
+        map))
+    ))
 
 (provide 'init-folding)
 ;;; init-folding.el ends here
