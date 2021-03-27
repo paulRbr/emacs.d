06fc3d1d9499b396fb8da013e59fc66ee278f0d5
Paul B <paul@bonaud.fr>
2021-03-27 13:57:58 +0100

    git: use consult instead of counsel for git-grep

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -56,9 +56,9 @@
     (add-hook 'magit-mode-hook (lambda () (local-unset-key [(meta h)])))))
 
 
-;; Convenient binding for counsel-git-grep (instead of vc-git-grep)
-(with-eval-after-load 'counsel
-  (define-key vc-prefix-map (kbd "f") 'counsel-git-grep))
+;; Convenient binding for consult-git-grep (instead of vc-git-grep)
+(with-eval-after-load 'consult
+  (define-key vc-prefix-map (kbd "f") 'consult-git-grep))
 
 
 
