bed285d5ddebb39182a0fbd7cf67a7b7f3760e4b
Paul B <paul@bonaud.fr>
2022-03-23 17:37:46 +0100

    trying to load persp projectile again

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -17,15 +17,15 @@
   (maybe-require-package 'ibuffer-projectile)
 
   (after-load 'projectile
-    ;; (when (maybe-require-package 'persp-projectile)
+    (when (maybe-require-package 'persp-projectile)
 
-    (persp-mode)
-    (require 'persp-projectile)
+      (persp-mode)
+      (require 'persp-projectile)
 
-    (define-key projectile-mode-map
-      (kbd "C-c p p")
-      'projectile-persp-switch-project)
-    ;; )
+      (define-key projectile-mode-map
+        (kbd "C-c p p")
+        'projectile-persp-switch-project)
+      )
     ;; )
     ;; (after-load 'projectile
     ;;   (when (maybe-require-package 'persp-mode)
